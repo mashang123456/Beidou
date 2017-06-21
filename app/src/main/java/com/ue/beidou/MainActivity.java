08f22ca7b586568fb3076875eb10c9a881e384a4
mashang123456 <865909475@qq.com>
2017-06-21 11:21:05 +0800

    根据百度地图开发指南添加注释

--- app/src/main/java/com/ue/beidou/MainActivity.java
+++ app/src/main/java/com/ue/beidou/MainActivity.java
@@ -2,6 +2,7 @@ package com.ue.beidou;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.MapStatus;
@@ -21,11 +23,27 @@ import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
+import java.util.List;
 
+import static com.baidu.location.LocationClientOption.LocationMode.Hight_Accuracy;
+
+/*
+* 目前国内主要有以下三种坐标系：
+1. WGS84：为一种大地坐标系，也是目前广泛使用的GPS全球卫星定位系统使用的坐标系；
+2. GCJ02：表示经过国测局加密的坐标；
+3. BD09：为百度坐标系，其中bd09ll表示百度经纬度坐标，bd09mc表示百度墨卡托米制坐标；
+Android定位SDK产品，支持全球定位，能够精准的获取经纬度信息。根据开发者的设置，
+在国内获得的坐标系类型可以是：国测局坐标、百度墨卡托坐标 和 百度经纬度坐标。
+在海外地区，只能获得WGS84坐标。请开发者在使用过程中注意坐标选择。
+* */
 public class MainActivity extends Activity {
 
     // 定位相关
-    LocationClient mLocClient;
+    /*
+    LocationClient类必须在主线程中声明，需要Context类型的参数Context需要时全进程有效的Context,
+    推荐用getApplicationConext获取全进程有效的Context。
+    * */
+    public LocationClient mLocClient;
     public MyLocationListenner myListener = new MyLocationListenner();
     private LocationMode mCurrentMode;
     BitmapDescriptor mCurrentMarker;
@@ -47,26 +65,29 @@ public class MainActivity extends Activity {
         mCurrentMode = LocationMode.NORMAL;
         btRequestLoc.setText("普通");
 
+        /*地图图层信息
+        * COMPASS 罗盘态，显示定位方向圈，保持定位图标在地图中心
+          FOLLOWING 跟随态，保持定位图标在地图中心
+          NORMAL 普通态： 更新定位数据时不对地图做任何操作
+        * */
         OnClickListener btnClickListener = new OnClickListener() {
             public void onClick(View v) {
                 switch (mCurrentMode) {
                     case NORMAL:
                         btRequestLoc.setText("跟随");
                         mCurrentMode = LocationMode.FOLLOWING;
-                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
-                                        mCurrentMode, true, mCurrentMarker));
+                        //参数一定位图显示模式；二是否显示方向信息；三用户自定义定位图标
+                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker));
                         break;
                     case COMPASS:
                         btRequestLoc.setText("普通");
                         mCurrentMode = LocationMode.NORMAL;
-                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
-                                        mCurrentMode, true, mCurrentMarker));
+                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(mCurrentMode, false, mCurrentMarker));
                         break;
                     case FOLLOWING:
                         btRequestLoc.setText("罗盘");
                         mCurrentMode = LocationMode.COMPASS;
-                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
-                                        mCurrentMode, true, mCurrentMarker));
+                        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker));
                         break;
                     default:
                         break;
@@ -78,20 +99,47 @@ public class MainActivity extends Activity {
         // 地图初始化
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
-
         mBaiduMap.setMyLocationEnabled(true);// 开启定位图层
-        mLocClient = new LocationClient(this);// 定位初始化
-        mLocClient.registerLocationListener(myListener);
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(1000);
-        mLocClient.setLocOption(option);
+
+        //第一步，初始化LocationClient类
+        //mLocClient = new LocationClient(this);// 定位初始化
+        mLocClient = new LocationClient(getApplicationContext());
+        mLocClient.registerLocationListener(myListener);//注册监听函数
+
+        //第二步，配置定位SDK参数
+        initLocation();
+        //第四步，开始定位
         mLocClient.start();
     }
 
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+
+        /*定位模式 分为高精度定位模式Hight_Accuracy 低功耗定位模式Battery_Saving 仅设备定位模式Device_Sensors
+        高精度定位模式：会同时使用网络定位和GPS定位，优先返回最高精度的定位结果；
+        低功耗定位模式：不会使用GPS，只会使用网络定位（Wi-Fi和基站定位）
+        仅用设备定位模式：不需要连接网络，只使用GPS进行定位，这种模式下不支持室内环境的定位
+        * */
+        option.setLocationMode(Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+
+        mLocClient.setLocOption(option);
+    }
     /**
-     * 定位SDK监听函数
+     * 第三步，实现BDLocationListener接口定位SDK监听函数
+     * BDLocationListener为结果监听接口，异步获取定位结果
      */
     public class MyLocationListenner implements BDLocationListener {
 
@@ -101,16 +149,107 @@ public class MainActivity extends Activity {
             if (location == null || mMapView == null) {
                 return;
             }
+            //打印出定位结果
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());    //获取定位时间
+
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());    //获取返回类型
+
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());    //获取纬度信息
+
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());    //获取经度信息
+
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());    //获取定位精准度
+
+            if (location.getLocType() == BDLocation.TypeGpsLocation){
+
+                // GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());    // 单位：公里每小时
+
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());    //获取卫星数
+
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());    //获取海拔高度信息，单位米
+
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());    //获取方向信息，单位度
+
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());    //获取地址信息
+
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+
+                // 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());    //获取地址信息
+
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());    //获取运营商信息
+
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+
+                // 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());    //位置语义化信息
+
+            List<Poi> list = location.getPoiList();    // POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+
+            Log.i("BaiduLocationApiDem", sb.toString());
+
+
+
+            //获取经纬度并显示在地图上
             MyLocationData locData = new MyLocationData.Builder()
                     .accuracy(location.getRadius())
                     // 此处设置开发者获取到的方向信息，顺时针0-360
                     .direction(100).latitude(location.getLatitude())
                     .longitude(location.getLongitude()).build();
+
             mBaiduMap.setMyLocationData(locData);
+
             if (isFirstLoc) {
                 isFirstLoc = false;
-                LatLng ll = new LatLng(location.getLatitude(),
-                        location.getLongitude());
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
                 MapStatus.Builder builder = new MapStatus.Builder();
                 builder.target(ll).zoom(18.0f);
                 mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
